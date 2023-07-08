74a22e4baa92b2dccce817305d72e4a2e695410f
NetAcad DEVASC <changeme@example.com>
2023-07-08 00:19:18 +0000

    Integrantes ET con commit

---
+++ Integrantes-ET.py
@@ -0,0 +1,10 @@
+I1 = "Cristhian Araya"
+I2 = "Sebastian Vivanco"
+I3 = "Stephan Leyton"
+
+print("===================================================")
+print("Nombres y apellidos de los integrantes del grupo")
+print("===================================================")
+print("-", I1)
+print("-", I2)
+print("-", I3)
